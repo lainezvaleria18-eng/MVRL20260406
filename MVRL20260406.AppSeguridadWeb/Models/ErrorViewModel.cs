2b9633d976d2ec3834d004e3f7c816dcee7fab54
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-04-06 11:11:55 -0600

    Agregar archivos de proyecto.

---
+++ MVRL20260406.AppSeguridadWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVRL20260406.AppSeguridadWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
