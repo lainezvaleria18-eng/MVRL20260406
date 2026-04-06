0d4fa1de99c0cfbb9a222eba6c8ecbefa0ca7b31
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-04-06 16:19:02 -0600

    monica cambio y modifico login.cshtml

--- MVRL20260406.AppSeguridadWeb/Models/Usuario.cs
+++ MVRL20260406.AppSeguridadWeb/Models/Usuario.cs
@@ -1,40 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-[Table("Usuarios")]
-public class Usuario
+namespace MVRL20260406.AppSeguridadWeb.Models
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
-    [StringLength(50)]
-    public string Login { get; set; }
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
+        [StringLength(50)]
+        public string? Login { get; set; }
 
-    [Required(ErrorMessage = "La contraseña es obligatoria")]
-    [StringLength(100)]
-    [DataType(DataType.Password)]
-    [Display(Name = "Contraseña")]
-    public string PasswordHash { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string? PasswordHash { get; set; }
 
-    [NotMapped]
-    [Required(ErrorMessage = "Debe confirmar la contraseña")]
-    [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden")]
-    [StringLength(100)]
-    [DataType(DataType.Password)]
-    [Display(Name = "Confirmar Contraseña")]
-    public string ConfirmarPassword { get; set; }
+        [NotMapped]
+        [Required(ErrorMessage = "Debe confirmar la contraseña")]
+        [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden")]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Contraseña")]
+        public string? ConfirmarPassword { get; set; }
 
-    [Required]
-    [StringLength(20)]
-    [AllowedValues("Administrador", "Editor", "Usuario", ErrorMessage = "Seleccione un rol válido")]
-    public string Rol { get; set; }
+        [Required]
+        [StringLength(20)]
+        [AllowedValues("Administrador", "Editor", "Usuario", ErrorMessage = "Seleccione un rol válido")]
+        public string? Rol { get; set; }
 
-    public bool EstaActivo { get; set; } = true;
+        public bool EstaActivo { get; set; } = true;
+    }
 }
-
-
-
-
-
